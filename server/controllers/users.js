fa78c721966159a41c76ad05d05307be502d0fb7
jkoone <thekoone123@gmail.com>
2019-02-25 10:46:49 -0500

    Class notes Monday 2/25 - Get/Post

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -2,17 +2,30 @@ const express = require("express");
 
 const conn = require("../models/mysql_connection");
 const app = express.Router();
+const userModel = require("../models/user");
 
 // Mapping routes to functions
 app.get("/", (req, res) => {
-  conn.query("SELECT * FROM InClass_Person", (err, data) => {
-      if(err) throw err;
-      res.send(data);
+  userModel.getAll((err, data) => {
+    if(err) throw err;
+    res.send(data);
   })
+  //conn.query("SELECT * FROM InClass_Person", (err, data) => {
+  //    if(err) throw err;
+  //    res.send(data);
+  //})
   //res.send([{FirstName: "Fake Person"}]);
-
 });
 
+app.post("/", (req, res) => {
+  userModel.add({}, (err, data) => {
+    if(err) throw err;
+    //
+  })
+})
+
+
+// Equivalent to return for require
 module.exports = app;
 
 
--- server/models/mysql_connection.js
+++ server/models/mysql_connection.js
@@ -7,13 +7,12 @@ const mysql = require("mysql");
 
 const conn = mysql.createPool({
   host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASS,
   database: process.env.MYSQL_DB,
   connectionLimit: 10,
-  insecureAuth: true,
-  debug: true
+  insecureAuth: true
 });
 
-console.log(conn);
 module.exports = conn;
